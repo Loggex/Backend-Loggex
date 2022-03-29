bb3d4b941594c40b1371c820562986e7bd7b8ffe
MiguelCandido <miguelcandidoperigo@gmail.com>
2022-03-29 10:47:31 -0300

    Scaffolding realizado

---
+++ API/LoggexWebAPI/LoggexWebAPI/Domains/Peca.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace LoggexWebAPI.Domains
+{
+    public partial class Peca
+    {
+        public Peca()
+        {
+            LogAlteracaos = new HashSet<LogAlteracao>();
+        }
+
+        public int IdPeca { get; set; }
+        public int? IdTipoPeca { get; set; }
+        public int? IdVeiculo { get; set; }
+        public bool EstadoPeca { get; set; }
+        public string ImgPeca { get; set; }
+
+        public virtual TiposPeca IdTipoPecaNavigation { get; set; }
+        public virtual Veiculo IdVeiculoNavigation { get; set; }
+        public virtual ICollection<LogAlteracao> LogAlteracaos { get; set; }
+    }
+}
