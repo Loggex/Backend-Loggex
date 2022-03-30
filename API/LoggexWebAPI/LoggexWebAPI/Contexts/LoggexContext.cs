b2380ee69a78aa8998c99fce298f917b5dd89e8b
MiguelCandido <miguelcandidoperigo@gmail.com>
2022-03-30 09:38:35 -0300

    Interfaces desenvolvidas

--- API/LoggexWebAPI/LoggexWebAPI/Contexts/LoggexContext.cs
+++ API/LoggexWebAPI/LoggexWebAPI/Contexts/LoggexContext.cs
@@ -35,7 +35,6 @@ namespace LoggexWebAPI.Contexts
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
                 optionsBuilder.UseSqlServer("Data Source=NOTE0111E6\\SQLEXPRESS; initial catalog=Loggex_BD; user Id=sa; pwd=Senai@132;");
             }
         }
